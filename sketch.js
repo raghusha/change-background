40fe59243a2b5a05484d154ddda1d7f5a53dad70
raghusha <88180468+raghusha@users.noreply.github.com>
2021-07-30 12:44:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+
+btn_red=createButton("red");
+btn_red.position(100,50);
+btn_red.mousePressed(red_bg);
+
+btn_green=createButton("green");
+btn_green.position(250,50);
+btn_green.mousePressed(green_bg);
+
+
+}
+
+function red_bg(){
+background('rgb(225,0,0)');
+}
+
+function green_bg(){
+background('rgb(0,225,0)');
+
+drawSprite();
+
+}
